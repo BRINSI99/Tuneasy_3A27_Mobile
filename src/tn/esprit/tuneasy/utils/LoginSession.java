5683befd6e2cf0ac12fa67d69ba409c5630fb807
BRINSI99 <youssef.brinsi@esprit.tn]>
2021-05-20 04:55:27 +0100

    Module_User

---
+++ src/tn/esprit/tuneasy/utils/LoginSession.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.tuneasy.utils;
+
+/**
+ *
+ * @author Maya
+ */
+public class LoginSession {
+   
+    public static int loggedUser = 0;
+    public static String emailUser = "";
+}
